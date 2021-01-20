61ca866fe328d0db7411ff338fb7afb064c897bf
aysemelek0 <aysemelek930@gmail.com>
2021-01-20 17:57:22 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.ad + " İsimli müşteri  sisteme eklendi.");
+
+        }
+
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine("Müşterilerin  bilgileri  listelendi. " + "İsim:" + musteri.ad + " Soyad: " + musteri.soyad +
+             " ID No:  " + musteri.Id + " Yaş:  " + musteri.yas);
+        }
+        public void Silme(Musteri musteri)
+        {
+            Console.WriteLine(musteri.ad + " İsimli müşteri sistemden silindi. ");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.ad = "Ayşe Melek";
+            musteri1.soyad = "Doğan";
+            musteri1.Id = 75287894;
+            musteri1.yas = 35;
+
+
+            Musteri musteri2 = new Musteri();
+            musteri2.ad = "Hamza";
+            musteri2.soyad = "Bayram";
+            musteri2.Id = 25789832;
+            musteri2.yas = 62;
+
+
+            Musteri musteri3 = new Musteri();
+            musteri3.ad = "Nesrin";
+            musteri3.soyad = "Kaya";
+            musteri3.Id = 87592145;
+            musteri3.yas = 29;
+
+            Console.WriteLine("Müşteri adı: " + musteri1.ad);
+            Console.WriteLine("Müşteri soyad: " + musteri1.soyad);
+            Console.WriteLine("Müşteri ID: " + musteri1.Id);
+            Console.WriteLine("Müşteri yaş: " + musteri1.yas);
+
+            Console.WriteLine("-------------------------------------------");
+
+            Console.WriteLine("Müşteri adı: " + musteri2.ad);
+            Console.WriteLine("Müşteri soyad: " + musteri2.soyad);
+            Console.WriteLine("Müşteri ID: " + musteri2.Id);
+            Console.WriteLine("Müşteri yaş: " + musteri2.yas);
+
+
+            Console.WriteLine("-------------------------------------------");
+
+
+            Console.WriteLine("Müşteri adı: " + musteri3.ad);
+            Console.WriteLine("Müşteri soyad: " + musteri3.soyad);
+            Console.WriteLine("Müşteri ID: " + musteri3.Id);
+            Console.WriteLine("Müşteri yaş: " + musteri3.yas);
+
+            Console.WriteLine("-------------------------------------------");
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Ekle(musteri3);
+            musteriManager.Listele(musteri1);
+            musteriManager.Listele(musteri2);
+            musteriManager.Listele(musteri3);
+            musteriManager.Silme(musteri1);
+            musteriManager.Silme(musteri2);
+            musteriManager.Silme(musteri3);
+        }
+    }
+        }
+            
